f88682e6aa8052fbef332ec2eb631ba6a37e12a2
ScriptDevs <elonmaskwife@gmail.com>
2025-08-21 14:27:54 +0800

    Update and rename NGUYENSPAWNER.lua to Speedhubx.lua

---
+++ Speedhubx.lua
@@ -0,0 +1,3 @@
+ID="311c86f8-b442-42d8-a381-3c9473b1f92a";
+loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
